fa27666c2e81854b389cead330da6830ded40dbe
Johny Junior <johnyjunior64@gmail.com>
2024-02-05 22:01:00 -0300

    build(cors): add cors for accept requests

--- index.ts
+++ index.ts
@@ -4,9 +4,12 @@ import { AppDataSource } from "./data-source";
 import { userRouter } from "./src/routes/user.router";
 import { ocurrenceRouter } from "./src/routes/occurence.router";
 import dotenv from "dotenv";
+import cors from "cors";
+
 dotenv.config();
 const app = express();
 app.use(json());
+app.use(cors());
 
 AppDataSource.initialize()
   .then(async () => {
